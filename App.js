8ba7dcb767c7d398eaec7ff948c61c7365f9651f
tiagotecinternet <tiago.tecinternet22@gmail.com>
2023-01-26 13:59:44 -0300

    Comentários e melhorias

--- App.js
+++ App.js
@@ -34,15 +34,31 @@ export default function App() {
 
     permissoesIos();
 
+
+    
+    /*** BRANCH 02: CÓDIGO PARA O EXEMPLO DE PUSH NOTIFICATION */
+
+    /* Para testar as 'push notifications', entre no site https://expo.dev/notifications
+    e preencha o campo "To (Expo push token from your app)" com o token do seu aparelho (veja no console/terminal), e coloque
+    também um título ("Message title") e um texto ("Message body") para a mensagem.
+    
+    Em seguida, clique em "Send a notifications" */
+
     /* Obter as permissões atuais do dispositivo */
     Notifications.getPermissionsAsync().then(status => {
       if(status.granted){
-        /* Permissões ok? Então vamos obter o token expo do aparelho */
+        /* Permissões ok? Então vamos obter o token expo do aparelho.
+        Este token é um código identificador único de cada aparelho que utilize um app através do Expo Go.
+        É através dele que a notificação será enviada. */
         Notifications.getExpoPushTokenAsync().then(token => {
           console.log(token);
         })
       }
     })
+
+    /*** BRANCH 02: FIM DO CÓDIGO PARA O EXEMPLO DE PUSH NOTIFICATION */
+
+
 
 
     /* Ouvinte de evento para as notificações recebidas, ou seja,
--- README.md
+++ README.md
@@ -8,10 +8,20 @@ https://docs.expo.dev/versions/latest/sdk/notifications/
 
 https://docs.expo.dev/push-notifications/overview/
 
+## Instalação
+
+`npx expo install expo-notifications`
+
 ## (Ferramenta de testes para envio de push notifications)
 
 https://expo.dev/notifications 
 
-## Instalação
+### Importante
 
-`npx expo install expo-notifications`
+Para utilizar o recurso de Push Notification (exemplo da branch 02-push), é necessário estar logado via linha de comando em sua conta do Expo.
+
+Para isso, instale o EAS CLI usando `npm install -g eas-cli` e em seguida execute `eas login`.
+
+Coloque seu e-mail e senha de sua conta Expo e pressione enter para logar.
+
+Caso não tenha cadastro no Expo, acesse: https://expo.dev/signup.
