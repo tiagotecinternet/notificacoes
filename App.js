79fddfbf63d15d6512c3654ca3be6eafdbc9ead7
tiagotecinternet <tiago.tecinternet22@gmail.com>
2023-01-26 09:02:00 -0300

    Instalação e configuração inicial

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, StatusBar, Button } from 'react-native';
+
+import * as Notifications from "expo-notifications";
+import { useEffect } from 'react';
 
 export default function App() {
+
+  useEffect( () => {
+
+    /* Ouvinte de evento para as notificações recebidas */
+    Notifications.addNotificationReceivedListener();
+    
+    /* Ouvinte de evento para as respostas dadas às notificações */
+    Notifications.addNotificationResponseReceivedListener();
+  
+  }, []);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <SafeAreaView style={styles.container}>
+        <Text>Exemplo de sistema de notificação local</Text>
+        <Button title='Disparar notificação' />
+      </SafeAreaView>
+    </>
   );
 }
 
